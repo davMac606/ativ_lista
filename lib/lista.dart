e4d0628bdcdbce5ef9c8eefd0e4fccc4330bc045
Arthur <cl202227@g.unicamp.br>
2023-08-29 09:59:26 -0300

    this shit is more beautiful than half of your work.

---
+++ lib/lista.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class ListWanna extends StatefulWidget {
+  const ListWanna({super.key});
+
+  @override
+  State<ListWanna> createState() => _ListWannaState();
+}
+
+class _ListWannaState extends State<ListWanna> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        title: const Text('Wannakry '),
+        
+      ),
+      body: ListView(children: [
+        ListTileTheme(child: ListTile(
+          leading: const Icon(Icons.signal_wifi_bad),
+          title: const Text('Eduroam'),
+          subtitle: const Text('WPA/WPA2'),
+          trailing: const Icon(Icons.wifi),
+          onTap: (){
+          },
+        ),
+        data: ListTileThemeData(
+          iconColor: Colors.red,
+          textColor: Colors.red,
+        ),   
+        ),  
+        ListTileTheme(child: ListTile(
+          leading: const Icon(Icons.wifi_password),
+          title: const Text('Roteando'),
+          subtitle: const Text('WPA/WPA2'),
+          trailing: const Icon(Icons.wifi_password),
+          onTap: (){},
+        ),
+        ), 
+
+
+
+      ]),
+    );
+  }
+}
