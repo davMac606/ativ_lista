9dd2173c2bbfdb90aa229179af22cd942d4614af
davMac606 <cl201238@g.unicamp.br>
2023-09-05 10:07:06 -0300

    adicionado o parametro 'status'

--- lib/lista.dart
+++ lib/lista.dart
@@ -17,54 +17,18 @@ class _ListWannaState extends State<ListWanna> {
         title: const Text('Wannakry '),
       ),
       body: ListView.separated(
-        separatorBuilder: (BuildContext context, int index) => Divider(thickness: 2),
+        separatorBuilder: (BuildContext context, int index) =>
+            Divider(thickness: 2),
         itemCount: 15,
         itemBuilder: (BuildContext context, int index) {
           return ListTile(
             leading: const Icon(Icons.person),
-            title:  const Text('Wannakry'),
+            title: const Text('Wannakry'),
             subtitle: const Text('Admin'),
             trailing: const Icon(Icons.shield),
           );
-        }, 
-        /*children: [
-        ListTileTheme(
-          child: ListTile(
-            leading: const Icon(Icons.signal_wifi_bad),
-            title: const Text('Eduroam'),
-            subtitle: const Text('WPA/WPA2'),
-            trailing: const Icon(Icons.wifi),
-            onTap: () {},
-          ),
-          data: ListTileThemeData(
-            iconColor: Colors.red,
-            textColor: Colors.red,
-          ),
-        ),
-        ListTileTheme(
-          child: ListTile(
-            leading: const Icon(Icons.wifi_password),
-            title: const Text('Roteando'),
-            subtitle: const Text('WPA/WPA2'),
-            trailing: const Icon(Icons.wifi_password),
-            onTap: () => {
-   
-            },
-          ),
-        ),
-        ListTileTheme(
-          child: ListTile(
-         leading: const Icon(Icons.signal_wifi_0_bar),
-         title: const Text("Virus.net"),
-         subtitle: const Text('Sem sinal'),
-         trailing: const Icon(Icons.wifi),   
-         onTap: () => {
-
-         },
-          )
-        ),
-        
-      ]*/),
+        },
+      ),
     );
   }
 }
--- lib/user.dart
+++ lib/user.dart
@@ -2,12 +2,16 @@
 class User {
   String _username;
   String _password;
+  String _status;
 
-  User(this._username, this._password);
+  User(this._username, this._password, this._status);
 
 
   get username => _username;
   get password => _password;
+  get status => _status;
   set username(value) => _username = username;
   set senha(value) => _password = password;
+  set status(value) => _status = value;
+
 }
