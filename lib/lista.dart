e2d1b0b65ec5deba4e9dd730edf0453160a18fe1
davMac606 <cl201238@g.unicamp.br>
2023-09-05 09:37:10 -0300

    deus obg pelo format

--- lib/lista.dart
+++ lib/lista.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 
@@ -13,36 +13,32 @@ class _ListWannaState extends State<ListWanna> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         title: const Text('Wannakry '),
-        
       ),
       body: ListView(children: [
-        ListTileTheme(child: ListTile(
-          leading: const Icon(Icons.signal_wifi_bad),
-          title: const Text('Eduroam'),
-          subtitle: const Text('WPA/WPA2'),
-          trailing: const Icon(Icons.wifi),
-          onTap: (){
-          },
+        ListTileTheme(
+          child: ListTile(
+            leading: const Icon(Icons.signal_wifi_bad),
+            title: const Text('Eduroam'),
+            subtitle: const Text('WPA/WPA2'),
+            trailing: const Icon(Icons.wifi),
+            onTap: () {},
+          ),
+          data: ListTileThemeData(
+            iconColor: Colors.red,
+            textColor: Colors.red,
+          ),
         ),
-        data: ListTileThemeData(
-          iconColor: Colors.red,
-          textColor: Colors.red,
-        ),   
-        ),  
-        ListTileTheme(child: ListTile(
-          leading: const Icon(Icons.wifi_password),
-          title: const Text('Roteando'),
-          subtitle: const Text('WPA/WPA2'),
-          trailing: const Icon(Icons.wifi_password),
-          onTap: (){},
+        ListTileTheme(
+          child: ListTile(
+            leading: const Icon(Icons.wifi_password),
+            title: const Text('Roteando'),
+            subtitle: const Text('WPA/WPA2'),
+            trailing: const Icon(Icons.wifi_password),
+            onTap: () {},
+          ),
         ),
-        ), 
-
-
-
       ]),
     );
   }
